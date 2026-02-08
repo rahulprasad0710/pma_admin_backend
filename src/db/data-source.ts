bb598db276193eb72b109f60f4809d635fb16764
rahulprasad0710 <raulshah2021@hotmail.com>
2026-02-08 21:49:39 +0545

    feat: add mastril ai feat

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -4,7 +4,7 @@ import APP_CONSTANT from "../constants/AppConfig";
 import { DataSource } from "typeorm";
 import entities from "./index";
 
-const dataSource = new DataSource({
+const AppDataSource = new DataSource({
     type: "postgres",
     host: APP_CONSTANT.DB_HOST,
     port: APP_CONSTANT.DB_PORT,
@@ -24,10 +24,29 @@ const dataSource = new DataSource({
     },
 });
 
+export const dataSource = new DataSource({
+    type: "postgres",
+    url: "postgresql://postgres:rahul99dbsuperbase@db.huzukjbojrvxsdrrlbzn.supabase.co:5432/postgres",
+
+    ssl: {
+        rejectUnauthorized: false, // REQUIRED for Supabase
+    },
+
+    synchronize: true,
+    logging: false,
+
+    entities: entities,
+    migrations: ["dist/migrations/*.js"],
+
+    extra: {
+        max: 5, // IMPORTANT: Supabase pgbouncer
+    },
+});
+
 export async function connectToDatabase() {
     try {
         const dbConnect = await dataSource.initialize();
-        console.log("Connected to database :-", dbConnect.options.database);
+        console.log("Connected to database :-", dbConnect.isInitialized);
     } catch (err: unknown) {
         console.error(err);
         // process.exit(1);
--- src/jobs/jobs.ts
+++ src/jobs/jobs.ts
@@ -1,7 +1,9 @@
 import APP_CONSTANT from "../constants/AppConfig";
 import { AdminAudit } from "../db/entity/AdminAudit";
 import { TEmail } from "../types/types";
+import { generateBookingSummary } from "../ai/bookingSummary.chain";
 import { getAdminAuditQueue } from "./AdminAudit.queue";
+import { getBookingAISummaryQueue } from "./bookingAISummary.queue";
 import { getEmailQueue } from "./emailQueue";
 import { insertAuditLog } from "../services/loggin.service";
 import { sendEmail } from "../config/email.config";
@@ -19,6 +21,7 @@ export const redisConfig = {
 async function startQueue() {
     await startEmailQueue();
     await startAdminAuditQueue();
+    await startBookingAISummaryQueue();
 }
 
 async function startEmailQueue() {
@@ -42,7 +45,7 @@ async function startEmailQueue() {
     emailQueue.on("failed", (job, err) => {
         console.error(
             `Job ID ${job.id} failed on attempt ${job.attemptsMade} of ${job.opts.attempts}. Error:`,
-            err
+            err,
         );
         // TODO: Implement error handling logic (e.g., alerting, retries)
         // For example, you could send an alert to the admin or retry the job
@@ -51,7 +54,43 @@ async function startEmailQueue() {
 
     emailQueue.on("completed", (job, result) => {
         console.log(
-            `✅ Job ID ${job.id} succeeded after ${job.attemptsMade} attempt(s).`
+            `✅ Job ID ${job.id} succeeded after ${job.attemptsMade} attempt(s).`,
+        );
+    });
+}
+
+async function startBookingAISummaryQueue() {
+    // Process jobs from the queue
+    const bookingAISummaryQueue = await getBookingAISummaryQueue();
+
+    bookingAISummaryQueue.process("booking-ai-summary-queue", async (job) => {
+        const bookingSummary = job.data;
+
+        try {
+            const response = await generateBookingSummary(bookingSummary);
+            console.log("LOG: ~ bookingAISummaryQueue ~ response:", response);
+            return response;
+        } catch (err: unknown) {
+            // TODO  ADD LOGGING
+            console.error(`Retrying job ${job.id} due to error:`, err);
+            throw err; // ! important: rethrow so Bull knows it failed DO NOT delete this line.
+        }
+    });
+
+    // Event listener for completed jobs
+    bookingAISummaryQueue.on("failed", (job, err) => {
+        console.error(
+            `Job ID ${job.id} failed on attempt ${job.attemptsMade} of ${job.opts.attempts}. Error:`,
+            err,
+        );
+        // TODO: Implement error handling logic (e.g., alerting, retries)
+        // For example, you could send an alert to the admin or retry the job
+    });
+    // Event listener for failed jobs
+
+    bookingAISummaryQueue.on("completed", (job, result) => {
+        console.log(
+            `✅ Job ID ${job.id} succeeded after ${job.attemptsMade} attempt(s).`,
         );
     });
 }
@@ -76,7 +115,7 @@ async function startAdminAuditQueue() {
     emailQueue.on("failed", (job, err) => {
         console.error(
             `Job ID ${job.id} failed on attempt ${job.attemptsMade} of ${job.opts.attempts}. Error:`,
-            err
+            err,
         );
         // TODO: Implement error handling logic (e.g., alerting, retries)
         // For example, you could send an alert to the admin or retry the job
@@ -85,7 +124,7 @@ async function startAdminAuditQueue() {
 
     emailQueue.on("completed", (job, result) => {
         console.log(
-            `✅ Job ID ${job.id} succeeded after ${job.attemptsMade} attempt(s).`
+            `✅ Job ID ${job.id} succeeded after ${job.attemptsMade} attempt(s).`,
         );
     });
 }
